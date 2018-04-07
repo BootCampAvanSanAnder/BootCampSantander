d53a64cf8a3919800205756f3e255f5a3a4fd7ba
Thiago Dias <thiagodbrt@gmail.com>
2018-04-06 21:39:58 -0300

    Contact as array, Okay?

--- src/app/input-page/input-page.component.ts
+++ src/app/input-page/input-page.component.ts
@@ -9,30 +9,24 @@ import { UserProfileModel } from '../models/user-profile.model';
 export class InputPageComponent implements OnInit {
 
   /*
-    Lista de usuários criados individualmente para
-    fins didáticos de demonstração do conceito de
-    binding de dados no Input de um component
+    Lista de usuários criada para fins didáticos
+    de demonstração do conceito de binding de dados
+    no Input de um component.
   */
-  public usuario1 = new UserProfileModel(
-    'Anderson de Mello',
-    'São Paulo, SP',
-    'Desenvolvedor Front-End Sênior');
-
-  public usuario2 = new UserProfileModel(
-    'Carlos Monteiro',
-    'Rio de Janeiro, RJ',
-    'Gerente de Tecnologia da Informação');
-
-  public usuario3 = new UserProfileModel(
-    'Adriana de Andrade',
-    'Guarulhos, SP',
-    'Coordenadora de Operações Financeiras');
-
-  /*
-    TODO: Criar os elementos de input com
-          o devido decorador mencionado
-          no curso.
-  */
+  public usuarios = [
+    new UserProfileModel(
+      'Anderson de Mello',
+      'São Paulo, SP',
+      'Desenvolvedor Front-End Sênior'),
+    new UserProfileModel(
+      'Carlos Monteiro',
+      'Rio de Janeiro, RJ',
+      'Gerente de Tecnologia da Informação'),
+    new UserProfileModel(
+      'Adriana de Andrade',
+      'Guarulhos, SP',
+      'Coordenadora de Operações Financeiras')
+  ];
 
   constructor() { }
 
