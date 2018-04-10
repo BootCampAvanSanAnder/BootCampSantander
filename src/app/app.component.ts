6f2f42a19a4f7480e6d0add4dd5f714126081add
José Gomes <jose.gomes.silva@accenture.com>
2018-04-10 11:41:19 -0300

    Formulário - template e reativo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  formulario: FormGroup;
+  constructor(private formBuilder: FormBuilder){}
+  onSubmit(){
+  }
+  ngOnInit(){
+    this.formulario = this.formBuilder.group({
+      nome: [null, Validators.required],
+      email: [null, [Validators.required, Validators.email ]]
+    })
+  }
+  verificaValidTouched(campo){
+    return !this.formulario.get(campo).valid && 
+            (this.formulario.get(campo).touched || this.formulario.get(campo).dirty);
+
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
