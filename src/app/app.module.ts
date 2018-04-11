1e4d1afc976b9c1ecb37e06f72a47c9057d8650d
Specie <suellyn.specie@accenture.com>
2018-04-11 18:40:32 -0300

    Aula - Requisitos - Estrutural - Testes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NovoComponenteComponent } from './novo-componente/novo-componente.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NovoComponenteComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/novo-componente/novo-componente.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-novo-componente',
+  templateUrl: './novo-componente.component.html',
+  styleUrls: ['./novo-componente.component.scss']
+})
+export class NovoComponenteComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
