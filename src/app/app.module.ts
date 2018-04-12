05ea78ee233a3911063cf1d6496a0b746303d3e0
Thiago Dias <thiagodbrt@gmail.com>
2018-04-12 01:02:29 -0300

    Removed solutions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { DummyService } from './service/dummy.service';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule
   ],
-  providers: [DummyService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
